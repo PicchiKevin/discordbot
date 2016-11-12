cf30196ae57f619430f502894f751c4b409f08a9
Picchi Kevin <kevin@picchi.ch>
2016-11-12 22:12:29 +0100

    progress adding permissions

--- commands.js
+++ commands.js
@@ -1,8 +1,13 @@
 var commands = {
   cmd_ping: function(message){
     console.log("Ping received from: " + message.author.username + "#" + message.author.discriminator);
-    message.reply("Pong");
-	}
+    message.reply("Pong " + message.guild.id);
+	},
+  cmd_test: function(message){
+    serverConfig = 'config/servers/' + message.guild.id + '.ini'
+    serverConfig.test.test = 'testing'
+    srvConfig = ini.parse(fs.readFileSync(serverConfig, 'utf-8'))
+  }
 }
 
 module.exports = commands;
--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js'),
+      util = require('util'),
+      client = new Discord.Client()
+
 var reload = require('require-reload'),
-		commands = reload('./commands.js')
-var fs = require('fs')
-, ini = require('ini')
-
-const client = new Discord.Client()
-
+    fs = require('fs'),
+    ini = require('ini'),
+    commands = reload('./commands.js')
 
 var config = ini.parse(fs.readFileSync('config/settings.ini', 'utf-8')) //Read config files with multiple secret things
 
+
+
 client.on('ready', () => {
   console.log('I am ready!')
+});
+
+
+client.on('guildCreate', (guild) => {
+  if (!fs.existsSync(guild.id)) {
+    fs.readFile('config/servers/template.ini', 'utf8', function (err,data) {
+      if(err) return console.log('[err] ' + err)
+
+      fs.writeFile('config/servers/' + guild.id + '.ini', data, function(err) {
+        if(err) return console.log('[err] ' + err)
+        console.log("[+++] Bot joined '"+guild.name+"' <"+guild.id+">")
+      });
+
+    });
+  }
 });
 
 client.on('message', message => {
   if (typeof commands['cmd_' + message.content] === 'function') { //checking if function exist
     commands['cmd_' + message.content](message)
   }else if(message.content == "reload"){
-			try{
-			commands = reload('./commands.js')
-		} catch (e) {
-		console.error("Failed to reload commands.js! Error: ", e);
-	}
-}
+    try{ commands = reload('./commands.js') }
+    catch (e) { console.error("[ERR] Failed to reload commands.js! Error: ", e) }
+  }
 });
+
+// client.on('debug', (message) => console.log(message));
 
 
 client.login(config.appdetails.secret)
