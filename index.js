590a69dc5fc1cd0eec874f5130933eef00f4145c
Picchi Kevin <kevin@picchi.ch>
2016-11-13 13:26:40 +0100

    bug fix, permission, json

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ var   reload = require('require-reload'),
       jsonfile = require('jsonfile'),
       commands = reload('./commands.js')
 
-var   config = ini.parse(fs.readFileSync('config/settings.ini', 'utf-8')) //Read config files with multiple secret things
+var config = jsonfile.readFileSync('config/settings.json')
+
+
+function havePerms(message, command, callback){
+  var result = false;
+  jsonfile.readFile('config/servers/' + message.guild.id + '.json', function(err, obj) {
+    if (typeof obj.users[message.author.id] !== 'undefined'){
+      for (var i = 0; i < obj.users[message.author.id].length; i++) {
+        if(obj.users[message.author.id][i] == command){
+          result = true
+        }
+      }
+    }
+    callback(result)
+  })
+}
+
 
 client.on('ready', () => {
   console.log('[~~~] Bot is ready !')
@@ -27,29 +43,21 @@ client.on('guildCreate', (guild) => {
 });
 
 client.on('message', message => {
-
-
   var command = message.content
   if (command.startsWith(config.appdetails.commandStart)){
     command = command.replace(/\n|\r\n|\r/g, '').split(' ')
-    command[0] = command[0].replace("!","") // just for making things tidy
+    command[0] = command[0].replace(config.appdetails.commandStart,"") // just for making things tidy
     console.log("[~~~] " + message.author.username + " issued command " + command[0]);
 
     if (typeof commands['cmd_' + command[0]] === 'function'){
       // console.log(message.guild.id + "  " + message.author.id + "  "  + 'cmd_' + command[0]);
-      var havePerms = false;
-
-      jsonfile.readFile('config/servers/' + message.guild.id + '.json', function(err, obj) {
-        if (typeof obj.users[message.author.id] !== 'undefined'){
-          for (var i = 0; i < obj.users[message.author.id].length; i++) {
-            console.log(obj.users[message.author.id][i] + " " + command[0]);
-            if(obj.users[message.author.id][i] == command[0]){
-               commands['cmd_' + command[0]](command, message)
-            }
-          }
+      havePerms(message, command[0], function(res){
+        if(res){
+          commands['cmd_' + command[0]](command, message)
+        }else{
+          message.reply("You don't have the permission to do this");
         }
-      })
-
+      });
 
     }else if (commands[0] == "reload") {
       try{ commands = reload('./commands.js') }
