93c10566d83c7f78c6e3b33ba23294a5eeb1632e
Picchi Kevin <kevin@picchi.ch>
2016-11-13 03:25:57 +0100

    Add/make permission working

--- commands.js
+++ commands.js
@@ -1,12 +1,17 @@
+var fs = require('fs'),
+    os = require("os")
+
 var commands = {
-  cmd_ping: function(message){
+  cmd_ping: function(command, message){
     console.log("Ping received from: " + message.author.username + "#" + message.author.discriminator);
     message.reply("Pong " + message.guild.id);
 	},
-  cmd_test: function(message){
-    serverConfig = 'config/servers/' + message.guild.id + '.ini'
-    serverConfig.test.test = 'testing'
-    srvConfig = ini.parse(fs.readFileSync(serverConfig, 'utf-8'))
+  cmd_test: function(commands, message){
+    var file = fs.readFileSync('config/servers/' + message.guild.id + '.json', 'utf8')
+    //var lineNbr = file.split(/[\r\n]/).map(function (line, l) { if (/(\[roles\])+/g.test(line)){ return l + 1 } })
+
+    console.log('test');
+    // processInput('config/servers/' + message.guild.id + '.json', "testligne" , lineNbr)
   }
 }
 
--- index.js
+++ index.js
@@ -2,40 +2,60 @@ const Discord = require('discord.js'),
       util = require('util'),
       client = new Discord.Client()
 
-var reload = require('require-reload'),
-    fs = require('fs'),
-    ini = require('ini'),
-    commands = reload('./commands.js')
+var   reload = require('require-reload'),
+      fs = require('fs'),
+      ini = require('ini'),
+      jsonfile = require('jsonfile'),
+      commands = reload('./commands.js')
 
-var config = ini.parse(fs.readFileSync('config/settings.ini', 'utf-8')) //Read config files with multiple secret things
-
-
+var   config = ini.parse(fs.readFileSync('config/settings.ini', 'utf-8')) //Read config files with multiple secret things
 
 client.on('ready', () => {
-  console.log('I am ready!')
+  console.log('[~~~] Bot is ready !')
 });
-
 
 client.on('guildCreate', (guild) => {
   if (!fs.existsSync(guild.id)) {
-    fs.readFile('config/servers/template.ini', 'utf8', function (err,data) {
-      if(err) return console.log('[err] ' + err)
-
-      fs.writeFile('config/servers/' + guild.id + '.ini', data, function(err) {
-        if(err) return console.log('[err] ' + err)
+    fs.readFile('config/servers/template.json', 'utf8', function (err,data) {
+      if (err) return console.log('[err] ' + err)
+      fs.writeFile('config/servers/' + guild.id + '.json', data, function(err) {
+        if (err) return console.log('[err] ' + err)
         console.log("[+++] Bot joined '"+guild.name+"' <"+guild.id+">")
       });
-
     });
   }
 });
 
 client.on('message', message => {
-  if (typeof commands['cmd_' + message.content] === 'function') { //checking if function exist
-    commands['cmd_' + message.content](message)
-  }else if(message.content == "reload"){
-    try{ commands = reload('./commands.js') }
-    catch (e) { console.error("[ERR] Failed to reload commands.js! Error: ", e) }
+
+
+  var command = message.content
+  if (command.startsWith(config.appdetails.commandStart)){
+    command = command.replace(/\n|\r\n|\r/g, '').split(' ')
+    command[0] = command[0].replace("!","") // just for making things tidy
+    console.log("[~~~] " + message.author.username + " issued command " + command[0]);
+
+    if (typeof commands['cmd_' + command[0]] === 'function'){
+      // console.log(message.guild.id + "  " + message.author.id + "  "  + 'cmd_' + command[0]);
+      var havePerms = false;
+
+      jsonfile.readFile('config/servers/' + message.guild.id + '.json', function(err, obj) {
+        if (typeof obj.users[message.author.id] !== 'undefined'){
+          for (var i = 0; i < obj.users[message.author.id].length; i++) {
+            console.log(obj.users[message.author.id][i] + " " + command[0]);
+            if(obj.users[message.author.id][i] == command[0]){
+               commands['cmd_' + command[0]](command, message)
+            }
+          }
+        }
+      })
+
+
+    }else if (commands[0] == "reload") {
+      try{ commands = reload('./commands.js') }
+      catch (e) { console.error("[ERR] Failed to reload commands.js! Error: ", e) }
+      console.log("reloaded");
+    }
   }
 });
 
